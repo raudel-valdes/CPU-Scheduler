a912797afe83a611a7563d64839f3914ced6cf00
Raudel <raudel.developer@gmail.com>
2020-02-06 22:23:40 -0500

    setting up bare bones of the project

---
+++ sched.c
@@ -0,0 +1,326 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+
+typedef struct Node{
+  char *word;
+  int count;
+  struct Node *next;
+  struct Node *prev;
+} Node;
+
+typedef struct List {
+  Node *head;
+  Node *tail;
+} List;
+
+void insertNodeAtTail(List *, char **, int);
+void sortList(List *);
+void swapAdjNodes(List **, Node **, Node **);
+void mergeLists(List *, List*, List *);
+void printList(List *, int);
+void printListToFile(List *, FILE **);
+void destroyList(List *);
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 4) {
+
+    printf("Please include the two input text file names and the output file name at execution time! \n");
+    exit(EXIT_FAILURE);
+
+  }
+
+  List firstFileList;
+  List secondFileList;
+  List thirdFileList;
+
+  firstFileList.head = NULL;
+  firstFileList.tail = NULL;
+
+  secondFileList.head = NULL;
+  secondFileList.tail = NULL;
+
+  thirdFileList.head = NULL;
+  thirdFileList.tail = NULL;
+
+  FILE *fPtr1;
+  FILE *fPtr2;
+  FILE *fPtr3;
+
+  char *scannedWord = NULL;
+  
+  fPtr1 = fopen(argv[1], "r");
+
+  if(fPtr1 == NULL) {
+
+    printf("The file %s was not found or could not be open. Please try again!", argv[1]);
+    exit(EXIT_FAILURE);
+
+  }
+
+  fPtr2 = fopen(argv[2], "r");
+
+  if(fPtr2 == NULL) {
+
+    printf("The file %s was not found or could not be open. Please try again!", argv[2]);
+    exit(EXIT_FAILURE);
+
+  }
+
+  fPtr3 = fopen(argv[3], "w+");
+
+  if(fPtr3 == NULL) {
+
+    printf("The file %s was not created or could not be open. Please try again!", argv[3]);
+    exit(EXIT_FAILURE);
+
+  }
+
+  while(fscanf(fPtr1,"%ms", &scannedWord) != EOF)
+    insertNodeAtTail(&firstFileList, &scannedWord, 1);
+
+  while(fscanf(fPtr2,"%ms", &scannedWord) != EOF)
+    insertNodeAtTail(&secondFileList, &scannedWord, 1);
+  
+  sortList(&firstFileList);
+  sortList(&secondFileList);
+
+  mergeLists(&firstFileList, &secondFileList, &thirdFileList);
+  printListToFile(&thirdFileList, &fPtr3);
+
+  destroyList(&firstFileList);
+  destroyList(&secondFileList);
+  destroyList(&thirdFileList);
+
+  fclose(fPtr1);
+  fclose(fPtr2);
+  fclose(fPtr3);
+
+  return 0;
+}
+
+void insertNodeAtTail(List *firstFileList, char **scannedWord, int wordCount) {
+
+  char *wordToInsert = strndup(*scannedWord, strlen(*scannedWord));
+  Node *nextTailNode = malloc(sizeof(Node));
+
+  nextTailNode->word = wordToInsert;
+  nextTailNode->count = wordCount;
+
+  Node *currentNode = firstFileList->head;
+  Node *currentTailNode = firstFileList->tail;
+
+  if (currentNode == NULL) {
+
+    nextTailNode->prev = NULL;
+    nextTailNode->next = NULL;
+
+    firstFileList->head = nextTailNode;
+    firstFileList->tail = nextTailNode;
+
+  } else {
+ 
+    while(currentNode != NULL) {
+      
+      if (strcmp(currentNode->word, wordToInsert) == 0) {
+
+        currentNode->count++;
+
+        free(nextTailNode->word);
+        free(nextTailNode);
+
+        break;
+        
+      } else 
+        currentNode = currentNode->next;
+
+      if (currentNode == NULL) {
+
+        nextTailNode->prev = currentTailNode;
+        nextTailNode->next = NULL;
+        currentTailNode->next = nextTailNode;
+        firstFileList->tail = nextTailNode;
+
+      }
+
+    }
+  
+  }
+
+  free(*scannedWord);
+}
+
+
+
+void sortList(List *unsortedList) {
+
+  Node *marker = NULL;
+  Node *markerPrev = NULL;
+  Node *compareNode = NULL;
+  Node *originalSwap = NULL;
+
+  markerPrev = unsortedList->head;
+  marker = unsortedList->head->next;  
+
+  while(marker != NULL && markerPrev != NULL) {
+
+    if (strcmp(markerPrev->word, marker->word) < 0) {
+
+      marker = marker->next;
+      markerPrev = markerPrev->next;
+
+    } else { 
+
+      swapAdjNodes(&unsortedList, &markerPrev, &marker);
+
+      originalSwap = marker;
+      marker = markerPrev->next;
+      compareNode = originalSwap->prev;
+      
+      while(compareNode != NULL && originalSwap != NULL && (strcmp(compareNode->word, originalSwap->word) > 0)) {
+       
+        swapAdjNodes(&unsortedList, &compareNode, &originalSwap);
+        compareNode = originalSwap->prev;
+      }
+
+      if (marker != NULL)
+        markerPrev = marker->prev;
+
+    }
+  }
+
+}
+
+void swapAdjNodes(List **unsortedList, Node **nodeOne, Node **nodeTwo) {
+
+  Node *tempNode = NULL;
+  tempNode = (*nodeOne)->prev;
+
+  if(tempNode != NULL) {
+
+    tempNode->next = (*nodeTwo);
+    (*nodeTwo)->prev = tempNode;
+    (*nodeOne)->next = (*nodeTwo)->next;
+    (*nodeTwo)->next = (*nodeOne);
+
+  } else {
+
+    (*nodeTwo)->prev = tempNode;
+    (*nodeOne)->next = (*nodeTwo)->next;
+    (*nodeTwo)->next = (*nodeOne);
+    (*unsortedList)->head = (*nodeTwo);
+
+  }
+
+  tempNode = (*nodeOne)->next;
+
+  if(tempNode != NULL) {
+
+    tempNode->prev = (*nodeOne);
+    (*nodeOne)->prev = (*nodeTwo);
+
+  } else {
+
+    (*nodeOne)->prev = (*nodeTwo);
+    (*unsortedList)->tail = (*nodeOne);
+
+  }
+
+}
+
+void mergeLists(List *sortedListOne, List *sortedListTwo, List *sortedListThree) {
+ 
+  Node *listOneNode = NULL;
+  Node *listTwoNode = NULL;
+  char *tempWord = NULL;
+
+  listOneNode = sortedListOne->head;
+  listTwoNode = sortedListTwo->head;
+  int totalCount = 0;
+
+  while (listOneNode != NULL && listTwoNode != NULL) {
+
+    if(strcmp(listOneNode->word, listTwoNode->word) == 0) {
+
+      tempWord = strndup(listOneNode->word, strlen(listOneNode->word));
+      totalCount = listOneNode->count + listTwoNode->count;
+      insertNodeAtTail(sortedListThree, &tempWord, totalCount);
+
+      listOneNode = listOneNode->next;
+      listTwoNode = listTwoNode->next;
+
+    } else if (strcmp(listOneNode->word, listTwoNode->word) < 0) {
+
+      listOneNode = listOneNode->next;
+
+    } else {
+
+      listTwoNode = listTwoNode->next;
+
+    }
+  }
+}
+
+void printList(List *list, int reverse) {
+
+  Node *currentNode = NULL;
+
+  if (!reverse) {
+    currentNode = list->head;
+    
+    while (currentNode != NULL) {
+
+      printf("%s,%d\n", currentNode->word, currentNode->count);
+      currentNode = currentNode->next;
+
+    }
+
+  } else {
+    
+    currentNode = list->tail;
+    
+    while (currentNode != NULL) {
+
+      printf("%s,%d\n", currentNode->word, currentNode->count);
+      currentNode = currentNode->prev;
+
+    }
+
+  }
+
+}
+
+void printListToFile(List *listToPrint, FILE **filePtr) {
+  
+  Node *traverseNode = NULL;
+  traverseNode = listToPrint->head;
+
+  while(traverseNode != NULL) {
+
+    fprintf((*filePtr), "%s,%d\n", traverseNode->word, traverseNode->count);
+    traverseNode = traverseNode->next;
+
+  }
+}
+
+void destroyList(List *listToDestroy) {
+
+  Node *nodeToDestroy = NULL;
+  Node *tempNode = NULL;
+
+  nodeToDestroy = listToDestroy->head;
+    
+  while(nodeToDestroy != NULL) {
+
+    tempNode = nodeToDestroy->next;
+
+    free(nodeToDestroy->word);
+    free(nodeToDestroy);
+
+    nodeToDestroy = tempNode;
+
+  }
+}
