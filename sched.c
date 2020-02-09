20e97e437653efd778977d0aac8ff26b26960532
Raudel <raudel.developer@gmail.com>
2020-02-08 23:27:52 -0500

    added new functions in order to carry out and break down the different tasks that the preemptive priority scheduling algorithm requires

--- sched.c
+++ sched.c
@@ -3,6 +3,8 @@ IMPORTANT QUESTIONS:
 
 1) Do we need to allocate memory for the integers or only for the nodes?
   -Do we need to worry about very large numbers? Should we use longs instead of ints?
+
+2) If there is a tie for PP, then FCFS’s rule will be used to break the tie.
 */
 
 #include <stdio.h>
@@ -21,6 +23,8 @@ typedef struct Node{
   int waitTime;
   struct Node *next;
   struct Node *prev;
+  struct Node *readyNext;
+  struct Node *readyPrev;  
 } Node;
 
 typedef struct List {
@@ -29,11 +33,15 @@ typedef struct List {
 } List;
 
 void insertNodeAtTail(List *, int, int, int, int);
+void insertReadyNodeAtTail(List *, Node **);
+Node * removeReadyNode(List*);
+void sortList(List*, int);
 void implementFCFS(List *);
-void implementPP(List *, long);
+void implementPP(List *);
 void printList(List *, int);
 void printListToFile(List *, FILE **);
 void destroyList(List *);
+
 
 int main(int argc, char *argv[]) {
 
@@ -108,10 +116,12 @@ int main(int argc, char *argv[]) {
   }
 
   // if (argv[3] == "FCFS") 
-  //   implementFCFS(&fileList);
-  // else implementPP(&fileList);
+  //  implementFCFS(&fileList);
+  // else{
+    implementPP(&fileList);
+    sortList(&fileList, 0);
+  //} 
 
-  implementFCFS(&fileList);
   printListToFile(&fileList, &fPtr2);
 
   destroyList(&fileList);
@@ -146,11 +156,90 @@ void insertNodeAtTail(List *fileList, int pid, int arrvTime, int burstTime, int 
     fileList->tail = nextTailNode;
 
   } else {
+
     currentTailNode->next = nextTailNode;
     nextTailNode->prev = currentTailNode;
     nextTailNode->next = NULL;
     fileList->tail = nextTailNode;    
+
   }
+
+}
+
+void insertReadyNodeAtTail(List * list, Node **nextTailNode) {
+
+  Node *currentHeadNode = list->head;
+  Node *currentTailNode = list->tail;
+
+  if (currentHeadNode == NULL) {
+
+    (*nextTailNode)->readyPrev = NULL;
+    (*nextTailNode)->readyNext = NULL;
+
+    list->head = nextTailNode;
+    list->tail = nextTailNode;
+
+  } else {
+
+    currentTailNode->readyNext = nextTailNode;
+    (*nextTailNode)->readyPrev = currentTailNode;
+    (*nextTailNode)->readyNext = NULL;
+    list->tail = nextTailNode;    
+
+  }
+
+}
+
+Node * removeReadyNode(List *list) {
+  
+  Node * nodeToRemove = list->tail;
+  Node * prevNode =  nodeToRemove->readyPrev;
+  
+  nodeToRemove->readyPrev = NULL;
+  prevNode->readyNext = NULL;
+  list->tail = prevNode;
+
+  return &nodeToRemove; 
+
+}
+
+void sortList(List *unsortedList, int sortByPriority) {
+
+  Node *marker = NULL;
+  Node *markerPrev = NULL;
+  Node *compareNode = NULL;
+  Node *originalSwap = NULL;
+
+  markerPrev = unsortedList->head;
+  marker = unsortedList->head->next;  
+
+  while(marker != NULL && markerPrev != NULL) {
+
+    if (strcmp(markerPrev->word, marker->word) < 0) {
+
+      marker = marker->next;
+      markerPrev = markerPrev->next;
+
+    } else { 
+
+      swapAdjNodes(&unsortedList, &markerPrev, &marker);
+
+      originalSwap = marker;
+      marker = markerPrev->next;
+      compareNode = originalSwap->prev;
+      
+      while(compareNode != NULL && originalSwap != NULL && (strcmp(compareNode->word, originalSwap->word) > 0)) {
+       
+        swapAdjNodes(&unsortedList, &compareNode, &originalSwap);
+        compareNode = originalSwap->prev;
+      }
+
+      if (marker != NULL)
+        markerPrev = marker->prev;
+
+    }
+  }
+
 }
 
 void implementFCFS(List *list) {
@@ -183,7 +272,79 @@ void implementFCFS(List *list) {
 
 }
 
-void implementPP(List *list, long plimit) {
+void implementPP(List *list) {
+
+  Node *processingNode = NULL;
+  Node *pArrv = NULL;
+
+  List *readyList = malloc(sizeof(List));
+  readyList->head = NULL;
+  readyList->tail = NULL;
+
+  int remainingBurst = 0;
+  int tcounter = 0;
+  int nextpArrvt = 0;
+
+  int pid = 0;
+  int arrvTime = 0;
+  int burstTime = 0;
+  int priority = 0;
+
+  processingNode = list->head;
+  pArrv = processingNode->next;
+  nextpArrvt = pArrv->arrvTime;
+
+  while(processingNode != NULL) {
+    
+    while(remainingBurst > 0) {
+
+      if(nextpArrvt == tcounter) {
+
+        if(processingNode->priority < pArrv->priority) {
+          
+          processingNode->remainingBurstTime = remainingBurst;
+
+          insertReadyNodeAtTail(&readyList, &processingNode);
+          sortList(&readyList, 1);
+          
+          processingNode = pArrv;
+          remainingBurst = processingNode->burstTime;
+
+        } else {
+
+          insertReadyNodeAtTail(&readyList, &pArrv);
+          sortList(&readyList, 1);
+
+        }
+
+        pArrv = pArrv->next;
+        nextpArrvt = pArrv->arrvTime;
+      }
+
+      remainingBurst--;
+      tcounter++;
+
+    }
+
+    processingNode = processingNode->next;
+
+    if(processingNode->priority < readyList->tail->priority) {
+
+      pid = processingNode->pid;
+      arrvTime = processingNode->arrvTime;
+      burstTime = processingNode->burstTime;
+      priority = processingNode->priority;
+      
+      processingNode = removeReadyNode(&readyList);
+
+    }
+
+    if(processingNode == NULL && readyList != NULL)
+      processingNode = removeReadyNode(&readyList);
+
+  }
+
+  destroyList(&readyList);
   
 }
 
@@ -192,6 +353,7 @@ void printList(List *list, int reverse) {
   Node *traverseNode = NULL;
 
   if (!reverse) {
+
     traverseNode = list->head;
     
     while (traverseNode != NULL) {
